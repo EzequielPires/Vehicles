ce1883b83dedbcdf19f826197bb56d2f7e770c93
EzequielPires <ezequiel.pires082000@gmail.com>
2022-04-26 15:31:16 -0300

    Commit

--- src/pages/integracoes/index.tsx
+++ src/pages/integracoes/index.tsx
@@ -167,6 +167,7 @@ export default function Integracoes() {
                             )}
                         />
                     </Modal.Footer>
+                    
                 </Modal>
             </div>
         </div>
